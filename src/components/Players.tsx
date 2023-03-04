8e631dc2f961055b615602f94f54144d72346575
gkosakow <gkosakow@umich.edu>
2023-03-03 23:58:49 -0500

    player names now being updated by TextFields

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -2,32 +2,38 @@ import { useState } from "react";
 import { Container, Stack, TextField, Divider } from "@mui/material";
 
 interface Player {
-	riotID: string;
+	playerNum: number,
+	riotID: string
 }
 
 const Players = () => {
 	const [players, setPlayers] = useState<Player[]>([
-		{ riotID: "Player 1" },
-		{ riotID: "Player 2" },
-		{ riotID: "Player 3" },
-		{ riotID: "Player 4" },
-		{ riotID: "Player 5" }
+		{ playerNum: 1, riotID: "Player 1" },
+		{ playerNum: 2, riotID: "Player 2" },
+		{ playerNum: 3, riotID: "Player 3" },
+		{ playerNum: 4, riotID: "Player 4" },
+		{ playerNum: 5, riotID: "Player 5" }
 	]);
 
-	function handleChangeUser(playerNum: number) {
-		// do stuff here
+	const handleChangePlayer = (e: string, playerNumSelected: number) => {
+		setPlayers(
+			players.map(player =>
+				player.playerNum === playerNumSelected ? { ...player, riotID: e } : player
+			))
 	}
+
+	console.log(players)
 
 	return (
 		<Container className="players">
 			<p>ROSTER</p>
 			<Divider />
 			<Stack className="player-stack" spacing={2}>
-				<TextField fullWidth label="Player 1" size="small" />
-				<TextField fullWidth label="Player 2" size="small" />
-				<TextField fullWidth label="Player 3" size="small" />
-				<TextField fullWidth label="Player 4" size="small" />
-				<TextField fullWidth label="Player 5" size="small" />
+				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 1)} label="Player 1" size="small" />
+				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 2)} label="Player 2" size="small" />
+				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 3)} label="Player 3" size="small" />
+				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 4)} label="Player 4" size="small" />
+				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 5)} label="Player 5" size="small" />
 			</Stack>
 		</Container>
 	)
