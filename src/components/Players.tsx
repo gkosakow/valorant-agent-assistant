edf16d75b8d31ebcb8f0c42a626ef7f2f0e95d43
gkosakow <gkosakow@umich.edu>
2023-03-04 00:37:31 -0500

    refactored for less repetition

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -22,18 +22,14 @@ const Players = () => {
 			))
 	}
 
-	console.log(players)
-
 	return (
 		<Container className="players">
 			<p>ROSTER</p>
 			<Divider />
 			<Stack className="player-stack" spacing={2}>
-				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 1)} label="Player 1" size="small" />
-				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 2)} label="Player 2" size="small" />
-				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 3)} label="Player 3" size="small" />
-				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 4)} label="Player 4" size="small" />
-				<TextField fullWidth onChange={(e) => handleChangePlayer(e.target.value, 5)} label="Player 5" size="small" />
+				{players.map(player => (
+					<TextField key={player.playerNum} fullWidth onChange={(e) => handleChangePlayer(e.target.value, player.playerNum)} label={`Player ${player.playerNum}`} size="small" autoComplete='off' />
+				))}
 			</Stack>
 		</Container>
 	)
