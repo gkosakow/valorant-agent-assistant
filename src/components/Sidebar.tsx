7d8969d1c588cc7e7f197b189342498bef5f70c3
gkosakow <gkosakow@umich.edu>
2023-03-21 22:50:00 -0400

    import fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import Players from "./Players";
 import Credits from "./Credits";
-import UserAuthentication from "./googleSignIn/userAuthentication";
+import UserAuthentication from "./googleSignIn/UserAuthentication";
 
 const Sidebar = () => {
 	return (
