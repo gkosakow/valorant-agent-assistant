350a96a885e83fd035aff40bbef111378db97a23
Greg Kosakowski <gkosakow@umich.edu>
2023-04-04 18:27:56 -0400

    fixed the way newUser is calculated

--- src/firebase/isNewUser.tsx
+++ src/firebase/isNewUser.tsx
@@ -1,6 +1,6 @@
 // checks if Firebase user is new by comparing their account creation time to their last sign in time
 export const isNewUser = (user: any) => {
-    if (user.metadata.createdAt === user.metadata.lastSignInTime) {
+    if (user.metadata.lastLoginAt - user.metadata.createdAt < 5) {
         // This is a new user, show welcome modal.
         console.log("NEW USER");
         console.log(user);
