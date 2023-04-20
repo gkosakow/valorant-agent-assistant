2208cdc7c9a787d4cea677c12952dbc69cf8454a
gkosakow <gkosakow@umich.edu>
2023-04-20 09:16:51 -0400

    Utility to save player data to firestore

---
+++ src/utilities/savePlayerToFirestore.tsx
@@ -0,0 +1,9 @@
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import { Player } from "../components/Player";
+
+export const savePlayerToFirestore = async (user: any, playerNum: number, player: Player) => {
+    const docRef = doc(db, "Users", `${user.uid}`, "Players", `player${playerNum}`);
+
+    await setDoc(docRef, { riotID: player.riotID, tagline: player.tagline }, { merge: true });
+}
