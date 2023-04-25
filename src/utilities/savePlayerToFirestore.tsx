aefaffff1da8f7cf7d7d2222d5268035329ca33c
gkosakow <gkosakow@umich.edu>
2023-04-25 03:14:37 -0400

    now updates all information about player

--- src/utilities/savePlayerToFirestore.tsx
+++ src/utilities/savePlayerToFirestore.tsx
@@ -5,5 +5,13 @@ import { Player } from "../components/Player";
 export const savePlayerToFirestore = async (user: any, playerNum: number, player: Player) => {
     const docRef = doc(db, "Users", `${user.uid}`, "Players", `player${playerNum}`);
 
-    await setDoc(docRef, { ...player, riotID: player.riotID, tagline: player.tagline }, { merge: true });
+    await setDoc(docRef, {
+        riotID: player.riotID,
+        tagline: player.tagline,
+        accLevel: player.accLevel,
+        bannerPhoto: player.bannerPhoto,
+        rank: player.rank,
+        rankIcon: player.rankIcon,
+        rr: player.rr
+    }, { merge: true });
 }
