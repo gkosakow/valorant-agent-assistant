b61ac70c60b41806873f83a4bd88ef604d30720b
Greg Kosakowski <gkosakow@umich.edu>
2023-05-16 12:27:50 -0400

    adding button to manually throw error

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,9 +1,15 @@
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
+import { Button } from '@mui/material';
+
+const causeError = () => {
+	console.error("Test");
+}
 
 const Credits = () => (
 	<div className="sidebar-box-centered">
 		<Breadcrumbs className="credits" separator='|' color="hover">
+			<Button sx={{ backgroundColor: 'black' }} onClick={causeError}>Test</Button>
 			<Link href="" color="inherit" underline="hover">Home</Link>
 			<Link href="" color="inherit" underline="hover">About</Link>
 			<Link href="https://github.com/gkosakow" color="inherit" underline="hover" target="_blank" rel="noreferrer">GitHub</Link>
