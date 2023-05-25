7e0f24cbfc875def25d0e5331c0713c4d78a69ee
gkosakow <gkosakow@umich.edu>
2023-05-25 03:00:47 -0400

    custom MUI Grid breakpoints

--- src/components/MapsPanel.tsx
+++ src/components/MapsPanel.tsx
@@ -1,7 +1,27 @@
 import { useState, useEffect } from "react";
 import { Box, Grid } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import MapCard from "./MapCard";
 import { retrieveMaps } from "../api/MapsAPI";
+
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+	breakpoints: {
+		values: { xs: 0, sm: 568, md: 600, lg: 1480, xl: 2000 }
+	}
+});
+
+declare module '@mui/material/styles' {
+	interface BreakpointOverrides {
+		xs: true; // removes the `xs` breakpoint
+		sm: true;
+		md: true;
+		lg: true;
+		xl: true;
+	}
+}
 
 // defining the Map interface to store the map id, name, and splash image
 export interface Map {
@@ -22,15 +42,17 @@ const MapsPanel = () => {
 	}, []);
 
 	return (
-		<Box className="map-grid">
-			<Grid container>
-				{mapList.map(map => (
-					<Grid item xs={12} md={12} lg={6} key={map.id}>
-						<MapCard map={map} />
-					</Grid>
-				))}
-			</Grid>
-		</Box>
+		<ThemeProvider theme={theme}>
+			<Box className="map-grid">
+				<Grid container>
+					{mapList.map(map => (
+						<Grid item xs={12} sm={12} md={12} lg={6} xl={4} key={map.id}>
+							<MapCard map={map} />
+						</Grid>
+					))}
+				</Grid>
+			</Box>
+		</ThemeProvider>
 	)
 }
 
