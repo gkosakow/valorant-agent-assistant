b1b39aed8c589375bf4e1022f1f8f0b8e88159a5
gkosakow <gkosakow@umich.edu>
2023-05-25 03:01:03 -0400

    removed test button

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -9,7 +9,6 @@ const causeError = () => {
 const Credits = () => (
 	<div className="sidebar-box-centered">
 		<Breadcrumbs className="credits" separator='|' color="hover">
-			<Button sx={{ backgroundColor: 'black' }} onClick={causeError}>Test</Button>
 			<Link href="" color="inherit" underline="hover">Home</Link>
 			<Link href="" color="inherit" underline="hover">About</Link>
 			<Link href="https://github.com/gkosakow" color="inherit" underline="hover" target="_blank" rel="noreferrer">GitHub</Link>
